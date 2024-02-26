f9582e1b636868c5764d52b6f24a35e69c109c6e
Diego Rodríguez Romero <110944222+di3g0r@users.noreply.github.com>
2024-02-26 12:59:20 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function miFuncion() {
+    window.alert("GitHub Viejito :)");
+}
+
+function busqueda(){
+    let searchInput= document.getElementById("search").value;
+    window.alert("Buscando el repo "+ searchInput);
+}
